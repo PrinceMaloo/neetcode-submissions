1acb05e2056f7360a879dc5680964f429cc4e1f1
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-08-21 00:30:24 +0530

    Update: clone-graph - submission-1

--- Data Structures & Algorithms/clone-graph/submission-1.py
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -1,30 +1,34 @@
-"""
-# Definition for a Node.
+
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
 
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
-            return None
+            return 
+
+        visit = {}
         
-        oldToNew = {}
-        oldToNew[node] = Node(node.val)
-        queue = deque([node])
+        def dfs(node):
+            if not node:
+                return node
+            
+            if node.val in visit:
+                return visit[node.val]
+            
+            print(node.val)
+            d_node = Node(node.val)
+            visit[d_node.val] = d_node
+            for neighbour_node in node.neighbors:
+                print('neighbour_node', neighbour_node.val)
+                if neighbour_node:
+                    d_node.neighbors.append(dfs(neighbour_node))
+            
+            return d_node
 
-        while(queue):
-            n = queue.popleft()
-            for nei in n.neighbors:
-                if nei not in oldToNew: 
-                    neiCopy = Node(nei.val)
-                    queue.append(nei)
-                    oldToNew[nei] = neiCopy
-
-                oldToNew[n].neighbors.append(oldToNew[nei])
-       
-        return oldToNew[node]
-
+        return dfs(node)
+                
+            
 
