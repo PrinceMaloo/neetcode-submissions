3c2deefd68e35d578db6befff64a92815bf313e6
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-08-22 10:55:47 +0530

    Update: pacific-atlantic-water-flow - submission-4

--- Data Structures & Algorithms/pacific-atlantic-water-flow/submission-4.py
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-4.py
@@ -1,30 +1,42 @@
 class Solution:
-    def pacificAtlantic(self, grid: List[List[int]]) -> List[List[int]]:
-        rows,cols = len(grid),len(grid[0])
-        pac, atl = set(),set()
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
         result = []
 
-        def dfs(r,c,visit,prevVal):
-            if (r,c) in visit or min(r,c) < 0 or r >= rows or c >= cols or grid[r][c]< prevVal:
-                return 
-            
-            visit.add((r,c))
-            dfs(r-1,c,visit,grid[r][c])
-            dfs(r+1,c,visit,grid[r][c])
-            dfs(r,c-1,visit,grid[r][c])
-            dfs(r,c+1,visit,grid[r][c])
-
-        for i in range(cols):
-            dfs(0,i,pac,grid[0][i])
-            dfs(rows  - 1,i,atl,grid[rows - 1][i])
-
-        for i in range(rows):
-            dfs(i,0,pac,grid[i][0])
-            dfs(i,cols-1,atl,grid[i][cols - 1])
+        pacific = set()
+        atlantic = set()
         
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
-                    result.append([r,c])
+        for i in range(rows):
+            pacific.add((i, 0))
+            atlantic.add((i, cols - 1))
+        
+        for i in range(cols):
+            pacific.add((0, i))
+            atlantic.add((rows - 1, i))
+
+        def dfs(r, c, sub_result):
+            if(min(r, c) < 0 or r == rows or c == cols):
+                return
+
+            sub_result.add((r,c))
+            neighbours = [[0,1], [0,-1], [1, 0], [-1, 0]]
+
+            for dr,dc in neighbours:
+                nr, nc = r + dr, c + dc
+                if ((nr,nc) not in sub_result and (0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c])):
+                    dfs(nr, nc, sub_result)
+                 
+            return
+        
+        for (i, j) in pacific.copy():
+            dfs(i,j, pacific)
+        
+        for (i, j) in atlantic.copy():
+            dfs(i, j, atlantic)
+        
+        for pair in pacific:
+            if pair in atlantic:
+                result.append(pair)
                 
         return result
+
