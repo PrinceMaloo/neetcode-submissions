5c2b4dca069b23e53bce5d5f508c518743156f4f
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-08-23 16:33:42 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #Cycle detection 
+        adjacency_list = defaultdict(list)
+
+        for n1, n2 in prerequisites:
+            adjacency_list[n1].append(n2)
+        
+        visit = set()
+        for i in range(numCourses):
+            if self.dfs(i, visit, cycle, adjacency_list):
+                return False
+            
+        return True
+    
+    def dfs(self, node, visit, cycle, adjacency_list):
+        if node in visit:
+            return True
+        
+        visit.add(node)
+
+        for neighbour in adjacency_list[node]:
+            if self.dfs(neighbour, visit, cycle, adjacency_list):
+                return True
+            
+        visit.remove(node)
+        adjacency_list[node] = []
+            
+        return False
+
+
+        
