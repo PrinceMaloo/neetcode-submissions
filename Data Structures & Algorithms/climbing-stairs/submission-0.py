456168c2ee2b53fb6d53bc598596e92657134040
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-08-30 17:19:25 +0530

    Update: climbing-stairs - submission-0

--- Data Structures & Algorithms/climbing-stairs/submission-0.py
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -1,18 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return self.fibonacci(n+1)
-    
-    def fibonacci(self,n):
-        if(n < 2):
-            return n
-        
-        dp = [0,1]
-        i = 2
-        while(i<=n):
-            temp = dp[1]
-            dp[1] = dp[0] + dp[1]
-            dp[0] = temp
-            i += 1
-
-        return dp[1]
+        dp = {}
+        def dfs(n):
+            if n < 0:
+                return 0
+            
+            if n <= 1:
+                return 1
+            
+            if n in dp:
+                return dp[n]
+            
+            dp[n] = dfs(n-1) + dfs(n-2)
+            return dp[n]
+            
+        return dfs(n)
         
