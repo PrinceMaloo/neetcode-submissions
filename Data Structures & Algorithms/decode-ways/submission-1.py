bb9972fa4bd973193a45cc5452de8b11070ffc35
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-09-10 19:23:33 +0530

    Update: decode-ways - submission-1

--- Data Structures & Algorithms/decode-ways/submission-1.py
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -1,33 +1,22 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:  
-        dp = {}
-        def dfs(i):
-            if(i == len(s)):
-                return 1
+    def numDecodings(self, s: str) -> int:
+        first, second = 1, 0
 
-            if(i in dp):
-                return dp[i]
 
-            if(not self.isValidString(s[i])):
-                return 0
+        def isvalid(string):
+            if 10 <= int(string) <= 26:
+                return True
             
-            res = dfs(i + 1)
-
-            if(i + 1 < len(s) and self.isValidString(s[i:i+2])):
-                res += dfs(i + 2)
-
-            dp[i] = res
-            return res
-
-        return dfs(0)             
-
-    def isValidString(self,s):
-        if(len(s) == 1 and s == '0'):
             return False
-        if(len(s) == 2 and (int(s) < 10 or int(s) > 26)):
-            return False
-        
-        return True
 
-         
+        for i in range(len(s)- 1, -1,-1):
+            temp = first
+
+            if s[i] == '0':
+                first = 0
+            elif i + 1 < len(s) and isvalid(s[i]+s[i+1]):
+                first += second
+            
+            second = temp
         
+        return first
