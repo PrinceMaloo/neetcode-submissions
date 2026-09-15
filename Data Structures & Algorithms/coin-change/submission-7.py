104907a4b292a1e0b5bc1f43f3ae6fb27f1773db
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2026-09-15 19:56:34 +0530

    Add: coin-change - submission-7

---
+++ Data Structures & Algorithms/coin-change/submission-7.py
@@ -0,0 +1,29 @@
+from collections import defaultdict 
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = defaultdict(int)
+
+        def dfs(i, amount):
+            if i >= len(coins):
+                return float("inf")
+
+            if amount == 0:
+                return dp[(i-1, amount)]
+
+            if (i, amount) in dp:
+                return dp[(i, amount)]
+
+            result = dfs(i+1, amount)
+            if amount - coins[i] >= 0:
+                result = min(result, 1 + dfs(i, amount - coins[i]))
+            
+            dp[(i, amount)] = result
+            return dp[(i, amount)]
+            
+            
+        result = dfs(0, amount)
+        return -1 if result == float("inf") else result
+        
+
+        
